50708d9cb176c4f538efbc106e8c80af2a91511d
Nic <gitnub.6q580@simplelogin.com>
2025-11-15 11:26:50 +0100

    Sort types

--- src/types/qortalRequests/types.ts
+++ src/types/qortalRequests/types.ts
@@ -1,127 +1,109 @@
-import { Service } from "../interfaces/resources"
+import { Service } from "../interfaces/resources";
 
-export type ForeignCoin =
-  | 'BTC'
-  | 'LTC'
-  | 'DOGE'
-  | 'DGB'
-  | 'RVN'
-  | 'ARRR'
+export type ForeignCoin = "BTC" | "LTC" | "DOGE" | "DGB" | "RVN" | "ARRR";
 
-  export type Coin =
-  | 'QORT'
-  | 'BTC'
-  | 'LTC'
-  | 'DOGE'
-  | 'DGB'
-  | 'RVN'
-  | 'ARRR'
+export type Coin = "QORT" | "BTC" | "LTC" | "DOGE" | "DGB" | "RVN" | "ARRR";
 
+export type ForeignBlockchain =
+  | "LITECOIN"
+  | "DOGECOIN"
+  | "BITCOIN"
+  | "DIGIBYTE"
+  | "RAVENCOIN"
+  | "PIRATECHAIN";
 
-  export type ForeignBlockchain =
-  | 'LITECOIN'
-  | 'DOGECOIN'
-  | 'BITCOIN'
-  | 'DIGIBYTE'
-  | 'RAVENCOIN'
-  | 'PIRATECHAIN'
+export type FeeType = "feekb" | "feeceiling";
+export type ConnectionType = "SSL" | "TCP";
 
-  export type FeeType = 'feekb' | 'feeceiling'
-  export type ConnectionType = 'SSL' | 'TCP'
+export interface CrosschainAtInfo {
+  qortalAtAddress: string;
+}
 
-  export interface CrosschainAtInfo {
-    qortalAtAddress: string;
-  }
-
-
-
-
-  export type ResourceToPublish =
+export type ResourceToPublish =
   | {
-      service: Service
-      identifier: string
-      name?: string
-      title?: string
-      description?: string
-      category?: string
-      tags?: string[]
-      base64: string
-      filename?: string
-      disableEncrypt?: boolean
+      service: Service;
+      identifier: string;
+      name?: string;
+      title?: string;
+      description?: string;
+      category?: string;
+      tags?: string[];
+      base64: string;
+      filename?: string;
+      disableEncrypt?: boolean;
     }
   | {
-      service: Service
-      identifier: string
-      name?: string
-      title?: string
-      description?: string
-      category?: string
-      tags?: string[]
-      data64: string
-      filename?: string
-      disableEncrypt?: boolean
+      service: Service;
+      identifier: string;
+      name?: string;
+      title?: string;
+      description?: string;
+      category?: string;
+      tags?: string[];
+      data64: string;
+      filename?: string;
+      disableEncrypt?: boolean;
     }
   | {
-      service: Service
-      identifier: string
-      name: string
-      title?: string
-      description?: string
-      category?: string
-      tags?: string[]
-      file: File
-      filename?: string
-      disableEncrypt?: boolean
-    }
+      service: Service;
+      identifier: string;
+      name: string;
+      title?: string;
+      description?: string;
+      category?: string;
+      tags?: string[];
+      file: File;
+      filename?: string;
+      disableEncrypt?: boolean;
+    };
 
+export interface ResourcePointer {
+  service: Service;
+  identifier: string;
+  name: string;
+}
 
-  export interface ResourcePointer {
-    service: Service
-    identifier: string
-    name: string
-  }
+export type ConfirmationStatus = "CONFIRMED" | "UNCONFIRMED" | "BOTH";
 
-  export type ConfirmationStatus = "CONFIRMED" | "UNCONFIRMED" | "BOTH"
-
-  export type TxType =
-  | 'GENESIS'
-  | 'PAYMENT'
-  | 'REGISTER_NAME'
-  | 'UPDATE_NAME'
-  | 'SELL_NAME'
-  | 'CANCEL_SELL_NAME'
-  | 'BUY_NAME'
-  | 'CREATE_POLL'
-  | 'VOTE_ON_POLL'
-  | 'ARBITRARY'
-  | 'ISSUE_ASSET'
-  | 'TRANSFER_ASSET'
-  | 'CREATE_ASSET_ORDER'
-  | 'CANCEL_ASSET_ORDER'
-  | 'MULTI_PAYMENT'
-  | 'DEPLOY_AT'
-  | 'MESSAGE'
-  | 'CHAT'
-  | 'PUBLICIZE'
-  | 'AIRDROP'
-  | 'AT'
-  | 'CREATE_GROUP'
-  | 'UPDATE_GROUP'
-  | 'ADD_GROUP_ADMIN'
-  | 'REMOVE_GROUP_ADMIN'
-  | 'GROUP_BAN'
-  | 'CANCEL_GROUP_BAN'
-  | 'GROUP_KICK'
-  | 'GROUP_INVITE'
-  | 'CANCEL_GROUP_INVITE'
-  | 'JOIN_GROUP'
-  | 'LEAVE_GROUP'
-  | 'GROUP_APPROVAL'
-  | 'SET_GROUP'
-  | 'UPDATE_ASSET'
-  | 'ACCOUNT_FLAGS'
-  | 'ENABLE_FORGING'
-  | 'REWARD_SHARE'
-  | 'ACCOUNT_LEVEL'
-  | 'TRANSFER_PRIVS'
-  | 'PRESENCE'
+export type TxType =
+  | "ACCOUNT_FLAGS"
+  | "ACCOUNT_LEVEL"
+  | "ADD_GROUP_ADMIN"
+  | "AIRDROP"
+  | "ARBITRARY"
+  | "AT"
+  | "BUY_NAME"
+  | "CANCEL_ASSET_ORDER"
+  | "CANCEL_GROUP_BAN"
+  | "CANCEL_GROUP_INVITE"
+  | "CANCEL_SELL_NAME"
+  | "CHAT"
+  | "CREATE_ASSET_ORDER"
+  | "CREATE_GROUP"
+  | "CREATE_POLL"
+  | "DEPLOY_AT"
+  | "ENABLE_FORGING"
+  | "GENESIS"
+  | "GROUP_APPROVAL"
+  | "GROUP_BAN"
+  | "GROUP_INVITE"
+  | "GROUP_KICK"
+  | "ISSUE_ASSET"
+  | "JOIN_GROUP"
+  | "LEAVE_GROUP"
+  | "MESSAGE"
+  | "MULTI_PAYMENT"
+  | "PAYMENT"
+  | "PRESENCE"
+  | "PUBLICIZE"
+  | "REGISTER_NAME"
+  | "REMOVE_GROUP_ADMIN"
+  | "REWARD_SHARE"
+  | "SELL_NAME"
+  | "SET_GROUP"
+  | "TRANSFER_ASSET"
+  | "TRANSFER_PRIVS"
+  | "UPDATE_ASSET"
+  | "UPDATE_GROUP"
+  | "UPDATE_NAME"
+  | "VOTE_ON_POLL";
